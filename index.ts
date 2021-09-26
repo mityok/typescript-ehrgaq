daf596d231bb1094346e2c04e4bbbbfda5e3b72d
mityok <ptdimitry@gmail.com>
2021-09-26 15:17:43 +0300

    initial game world

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import './style.css';
 import * as Phaser from 'phaser';
-import { PRINCESS } from './assets/assets';
+import { DUDE, PLATFORM } from './assets/assets';
 var config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
@@ -8,51 +8,71 @@ var config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 },
+      gravity: { y: 300 },
+      debug: false,
     },
   },
   scene: {
     preload: preload,
     create: create,
+    update: update,
   },
 };
+
+var platforms;
+
+var player;
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.setBaseURL('http://labs.phaser.io');
+  var shardsImg = new Image();
+  shardsImg.onload = () => {
+    this.textures.addSpriteSheet('dude', shardsImg, {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  };
+  shardsImg.src = DUDE;
+  this.textures.addBase64('ground', PLATFORM);
 
-  this.textures.once(
-    'addtexture',
-    function () {
-      this.add.image(40, 30, 'logo');
-    },
-    this
-  );
-
-  this.textures.addBase64('logo', PRINCESS);
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.image('sky', 'assets/sky.png');
+  //this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
 }
+
+const onClick = () => {
+  player.setVelocityX(160);
+
+  player.anims.play('right', true);
+};
 
 function create() {
-  this.add.image(400, 300, 'sky');
-
-  var particles = this.add.particles('red');
-
-  var emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD',
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
   });
+  player = this.physics.add.sprite(100, 450, 'dude');
+  player.body.setGravityY(300);
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+  this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'star');
+  this.add.image(40, 30, 'logo');
+  this.add.image(60, 60, 'ground');
 
-  var logo = this.physics.add.image(400, 100, 'logo');
+  platforms = this.physics.add.staticGroup();
 
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-  emitter.startFollow(logo);
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+  this.physics.add.collider(player, platforms);
+  this.input.on('pointerdown', onClick);
 }
+
+function update() {}
