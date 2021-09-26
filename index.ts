7287e3e5e63cdb178ee1e4185ff698cb79786917
mityok <ptdimitry@gmail.com>
2021-09-26 21:50:41 +0300

    camera follow

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import './style.css';
 import * as Phaser from 'phaser';
 import { DUDE, PLATFORM } from './assets/assets';
+
+//https://phaser.io/tutorials/making-your-first-phaser-3-game/part10
+//https://github.com/alexzimakov/phaser-hello-world/blob/master/src/master.js
+
+//https://stackabuse.com/phaser-3-and-tiled-building-a-platformer/
+//https://medium.com/@michaelwesthadley/modular-game-worlds-in-phaser-3-tilemaps-1-958fc7e6bbd6
+
 var config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
@@ -9,7 +16,7 @@ var config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 300 },
-      debug: false,
+      debug: true,
     },
   },
   scene: {
@@ -22,6 +29,9 @@ var config = {
 var platforms;
 
 var player;
+
+var score = 0;
+var scoreText;
 
 var game = new Phaser.Game(config);
 
@@ -37,15 +47,16 @@ function preload() {
   this.textures.addBase64('ground', PLATFORM);
 
   this.load.image('sky', 'assets/sky.png');
-  //this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
+
 }
 
 const onClick = () => {
-  player.setVelocityX(160);
+  if (player.body.touching.down) {
+    player.setVelocityY(-530);
+  }
+  //player.setVelocityX(160);
 
-  player.anims.play('right', true);
+  //player.anims.play('right', true);
 };
 
 function create() {
@@ -59,20 +70,27 @@ function create() {
   player.body.setGravityY(300);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
-  this.add.image(400, 300, 'sky');
-  this.add.image(400, 300, 'star');
-  this.add.image(40, 30, 'logo');
-  this.add.image(60, 60, 'ground');
 
   platforms = this.physics.add.staticGroup();
 
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-  platforms.create(600, 400, 'ground');
+  platforms.create(50, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
   this.physics.add.collider(player, platforms);
   this.input.on('pointerdown', onClick);
+  player.anims.play('right', true);
+  scoreText = this.add.text(16, 16, 'score: 0', {
+    fontSize: '32px',
+    fill: '#555',
+  });
+  player.setVelocityX(30);
+  this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2);
+        this.physics.world.setBounds(0, 0, 1920 * 2, 1080 * 2);
+  this.cameras.main.startFollow(player, true, 0.05, 0.05);
+  this.cameras.main.followOffset.set(-100, 0);
+
 }
 
 function update() {}
